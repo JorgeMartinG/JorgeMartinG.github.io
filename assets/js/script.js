82a17eddb1d07ecaf085f7ed960c210696d9ca80
Jorge Martín <jorgemartintdh@gmail.com>
2023-04-29 23:10:34 +0200

    movida

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+$(function () {    
+// Navigation 
+    $('.site-navigation').affix({
+      offset: {
+        top: $('.body').height()
+            }
+    });
+
+    var $window = $(window);
+    function checkWidth() {
+        var windowsize = $window.width();
+        if (windowsize < 768) {
+            $('.nav a').on('click', function(){
+                $('.navbar-toggle').click() //bootstrap 3.x by Richard
+            });
+        }
+    }
+    // Execute on load
+    checkWidth();
+    // Bind event listener
+    $(window).resize(checkWidth);
+
+// Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.site-header',
+        offset: 10
+    });
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(document).on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+}());
