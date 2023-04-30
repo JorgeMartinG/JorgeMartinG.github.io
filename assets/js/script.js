7eae2e8d671291d4792520fc28f25159ca262735
Jorge Martín <jorgemartintdh@gmail.com>
2023-04-30 19:08:05 +0200

    some changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,7 @@ $(function () {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 500, 'easeInOutExpo');
+        }, 800, 'easeInOutExpo');
         event.preventDefault();
     });
 }());
